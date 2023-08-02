76956af58a0b371700275cd88d89b86440c6df16
vitaovich <vitaliyalekhnovich@gmail.com>
2023-08-02 13:43:37 +0000

    update client for custom yahoo provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,25 +2,28 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
-  debug: true,
+  // debug: true,
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
     }),
     {
       id: "yahoo",
       name: "Yahoo",
       type: "oauth",
       wellKnown: "https://api.login.yahoo.com/.well-known/openid-configuration",
-      clientId: process.env.YAHOO_APP_ID,
-      clientSecret: process.env.YAHOO_CLIENT_ID,
-      checks: ['pkce', 'state'],
+      clientId: process.env.YAHOO_CLIENT_ID,
+      clientSecret: process.env.YAHOO_CLIENT_SECRET,
+      client: {
+        authorization_signed_response_alg: 'ES256',
+        id_token_signed_response_alg: 'ES256'
+      },
       authorization: {
         params: {
           client_id: process.env.YAHOO_CLIENT_ID,
           redirect_uri: process.env.YAHOO_REDIRECT_URI,
-          response_type: "code",
+          response_type: 'code',
         }
       },
       profile(profile) {
