7b194a4045f67e3c6a1b629f455ab4221bb6fc12
vitaovich <vitaliyalekhnovich@gmail.com>
2023-08-03 19:07:27 +0000

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,40 +8,13 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-function GetSecret() {
-  const mySecret: string | undefined = process.env.NEXT_PUBLIC_MY_SECRET;
-  const defaultValue: string = "no secret found";
-  return mySecret ?? defaultValue;
-}
-
-async function getNFLTeams() {
-  const res = await fetch(`api/teams`)
-  const json = await res.json();
-  console.log("CALL api/teams:" + JSON.stringify(json));
-  return json;
-}
-
 export default function Home() {
-  const mySecret = GetSecret();
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
-  const description = `My Secret: ${mySecret}`;
-
-  function handleClick() {
-    if (session) {
-      console.log("Active Session:" + JSON.stringify(session));
-    } else {
-      console.log("No Session:" + JSON.stringify(session));
-    }
-  }
-
   return (
     <>
-      <h1 className="text-3xl">
-        Starter NextJS TailwindCSS
-      </h1>
-      <div className="">{description}</div>
+
       <div className="flex flex-row justify-between m-4">
         {!session && (
           <>
@@ -63,17 +36,14 @@ export default function Home() {
         )}
         {session?.user && (
           <>
-            {/* {session.user.image && (
-              <span
-                style={{ backgroundImage: `url('${session.user.image}')` }}
-              />
-            )} */}
             {/* <Image
               src={session.user.image}
               alt="profile image"
               width={50}
               height={50}
             /> */}
+            <Link className='bg-blue-400 text-white py-4 px-2 rounded-md' href="/fantasycaller">Fantasy Caller</Link>
+
             <span >
               <small>Signed in as</small>
               <br />
@@ -91,13 +61,6 @@ export default function Home() {
             </a>
           </>
         )}
-        <Link href="/fantasycaller">Fantasy Caller</Link>
-        <button className='bg-gray-300 p-4' onClick={handleClick}>
-          Click Me
-        </button>
-        <button className='bg-gray-300 p-4' onClick={getNFLTeams}>
-          Get Teams
-        </button>
       </div>
     </>
   )
