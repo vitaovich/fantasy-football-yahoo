c019269cd62add0e9ae5bef4e0c6e2b2e0f619ae
vitaovich <vitaliyalekhnovich@gmail.com>
2023-08-06 05:12:47 +0000

    add image and fire animation with css example and tailwind example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from '@next/font/google'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
+import styles from '@/pages/index.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,8 @@ export default function Home() {
         {session?.user && (
           <>
             <Link className='bg-blue-400 text-white py-4 px-2 rounded-md' href="/fantasycaller">Fantasy Caller</Link>
+            <div className="w-[128px] h-[128px] bg-cover bg-fire animate-fire"></div>
+            <div className={styles.animation}></div>
             <Link className='bg-blue-400 text-white py-4 px-2 rounded-md' href="/league">League Info</Link>
           </>
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,18 @@ module.exports = {
     extend: {
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
+        fire: 'fire 2s steps(64) infinite'
+      },
+      backgroundImage: {
+        'fire': "url('/assets/IMG_0253.PNG')",
       },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-9deg)' },
           '50%': { transform: 'rotate(9deg)' },
+        },
+        fire: {
+          '100%': { 'background-position': '-8192px' },
         }
       },
     },
