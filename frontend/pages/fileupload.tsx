2a0d15c2dbcb79c69a43bef745184be73a580269
vitaovich <vitaliyalekhnovich@gmail.com>
2023-08-10 23:40:28 +0000

    add ability to upload to sas url

--- frontend/pages/fileupload.tsx
+++ frontend/pages/fileupload.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { BlockBlobClient } from '@azure/storage-blob';
+import { convertFileToArrayBuffer } from '@/lib/convert-file-to-arraybuffer';
 
 const FileUpload = () => {
     const [file, setFile] = useState<any>();
     const [result, setResult] = useState<string>();
+    const [sasURL, setSasURL] = useState<string>('');
 
     const onFileChange = (event: any) => {
         // Update the state
@@ -11,33 +14,36 @@ const FileUpload = () => {
     };
 
     async function onSasTokenHandler(name: string) {
-        if(!file) {
+        if (!file) {
             setResult("Must specify a file first.");
             return
         }
-        
+
         const res = await fetch('/api/azurefunctions', {
             method: "POST",
             body: JSON.stringify({ name: name, fileName: file.name })
         })
         const fantasySportsResult = await res.json();
+        setSasURL(fantasySportsResult.result);
         setResult(JSON.stringify(fantasySportsResult, null, 2));
     };
 
     const onFileUpload = () => {
-        // Create an object of formData
-        const formData = new FormData();
+        if (sasURL === '') return;
 
-        // Update the formData object
-        formData.append(
-            "myFile",
-            file,
-            file.name
-        );
+        convertFileToArrayBuffer(file as File)
+            .then((fileArrayBuffer) => {
+                if (
+                    fileArrayBuffer === null ||
+                    fileArrayBuffer.byteLength < 1 ||
+                    fileArrayBuffer.byteLength > 256000
+                )
+                    return;
 
-        // Details of the uploaded file
-        console.log(file);
-        console.log(formData);
+                console.log("uploading file");
+                const blockBlobClient = new BlockBlobClient(sasURL);
+                return blockBlobClient.uploadData(fileArrayBuffer);
+            })
     };
 
     return (
@@ -50,8 +56,14 @@ const FileUpload = () => {
                 </button>
                 {result && (
                     <div className="flex flex-row">
-                        <h1>SAS Token:</h1>
+                        <h1>Result:</h1>
                         <p>{result}</p>
+                    </div>
+                )}
+                {sasURL && (
+                    <div className="flex flex-row">
+                        <h1>SAS Token:</h1>
+                        <p>{sasURL}</p>
                     </div>
                 )}
                 <button className="bg-green-500 py-2 px-4 rounded-full text-white" onClick={onFileUpload}>
